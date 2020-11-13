96983262e5e99365fa94e50d06a2e00d89616ddb
RuttyJ <ruttyj92@hotmail.com>
2020-11-12 19:03:08 -0500

    .

--- server/Lib/HandleRoom.js
+++ server/Lib/HandleRoom.js
@@ -16,48 +16,41 @@ module.exports = function({
         if (isDef(roomCode)) {
             let room = roomManager.getRoomByCode(roomCode);
             if (isDef(room)) {
-            let personManager = room.getPersonManager();
-            if (isDef(personManager)) {
-                let myClientId = thisClientKey;
-                let person = personManager.getPersonByClientId(myClientId);
+                let personManager = room.getPersonManager();
+                if (isDef(personManager)) {
+                    let myClientId = thisClientKey;
+                    let person = personManager.getPersonByClientId(myClientId);
 
-                let newProps = {
-                    ...props,
-                    roomCode,
-                    thisClientKey: `${props.thisClientKey}`,
-                    thisRoomCode: roomCode,
-                    room,
-                    thisRoom: room,
-                    personManager,
-                };
-                if (isDef(person)) {
-                Object.assign(newProps, {
-                    person,
-                    thisPersonId: person.getId(),
-                    thisPerson: person,
-                });
+                    let newProps = {
+                        ...props,
+                        roomCode,
+                        thisClientKey: `${props.thisClientKey}`,
+                        thisRoomCode: roomCode,
+                        room,
+                        thisRoom: room,
+                        personManager,
+                    };
+                    if (isDef(person)) {
+                        Object.assign(newProps, {
+                            person,
+                            thisPersonId: person.getId(),
+                            thisPerson: person,
+                        });
+                    }
+                    responses = fn(newProps, checkpoints);
+
+                    if (isDef(responses)) {
+                        let clientPersonMapping = {};
+                        personManager.getConnectedPeople().forEach((person) => {
+                            clientPersonMapping[String(person.getClientId())] = true;
+                        });
+                        let clientIds = Object.keys(clientPersonMapping);
+                        reducedResponses.addToBucket(
+                            responses.reduce(myClientId, clientIds)
+                        );
+                    }
+                    return responses;
                 }
-                /*
-                console.log({
-                    props:      props.thisClientKey,
-                    newProps:   newProps.thisClientKey,
-                });
-                //*/
-                responses = fn(newProps, checkpoints);
-
-                if (isDef(responses)) {
-                    let clientPersonMapping = {};
-                    personManager.getConnectedPeople().forEach((person) => {
-                        clientPersonMapping[String(person.getClientId())] = true;
-                    });
-                    let clientIds = Object.keys(clientPersonMapping);
-
-                    reducedResponses.addToBucket(
-                        responses.reduce(myClientId, clientIds)
-                    );
-                }
-                return responses;
-            }
             }
         }
         if (!isDef(responses) && isFunc(fallback)) {
