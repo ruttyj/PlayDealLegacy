b89b77ef01026be929a3b0c9d9332439882b18f7
RuttyJ <ruttyj92@hotmail.com>
2020-11-18 21:26:25 -0500

    clean up server/index.js

--- server/index.js
+++ server/index.js
@@ -1,112 +1,48 @@
 #!/usr/bin/env node
-const serverFolder        = `.`
-const builderFolder       = `${serverFolder}/Builders`
-const utils               = require(`${serverFolder}/utils/index.js`)
-const buildPlaydealServer = require(`${builderFolder}/Objects/Server`)
+const serverFolder          = `.`
+const builderFolder         = `${serverFolder}/Builders`
+const buildPlaydealServer   = require(`${builderFolder}/Objects/Server`)
+const AppHelpers            = require(`${builderFolder}/Objects/App/AppHelpers`)
 
+const cookie                = require(`cookie`);
+const http                  = require(`http`);
+const utils                 = require(`${serverFolder}/utils/index.js`)
 
-const cookie  = require(`cookie`);
-const app     = require(`${serverFolder}/server.dev.js`);
-const debug   = require(`debug`)(`server:server`);
-const http    = require(`http`);
+const PlayDealServer        = buildPlaydealServer({utils});
+const CookieTokenManager    = require(`${serverFolder}/CookieTokenManager`);
+const cookieTokenManager    = CookieTokenManager.getInstance();
 
-const CookieTokenManager = require(`${serverFolder}/CookieTokenManager`);
-const cookieTokenManager = CookieTokenManager.getInstance();
-
-const { isDef } = require(`${serverFolder}/utils/helperMethods`);
+const app                         = require(`${serverFolder}/app.js`);
+const { normalizePort, onError }  = AppHelpers()
 
 /**
  * Get port from environment and store in Express.
  */
-
 const port = normalizePort(process.env.PORT || `3001`);
 app.set(`port`, port);
 
 /**
  * Create HTTP hostserver.
  */
+const server          = http.createServer(app);
+server.listen(port);
+server.on(`error`, onError);
 
-const server = http.createServer(app);
-const PlayDealServer = buildPlaydealServer({utils});
-const playDealServer = new PlayDealServer();
-
+/**
+ * Create Socket
+ * Initialize PlayDeal
+ */
 const io = require(`socket.io`)(server);
+const playDealServer  = new PlayDealServer();
 io.on(`connection`, (thisClient) => {
-
   // @TODO this could be moved into playDealServer
   // Associate socket to cookie
+  const { isDef } = utils;
   let cookies = cookie.parse(thisClient.request.headers.cookie);
   if (isDef(cookies.token) && isDef(thisClient.id)) {
     console.log(`associateTokenAndClient`, cookies.token, thisClient.id);
     cookieTokenManager.associateTokenAndClient(cookies.token, thisClient.id);
   }
-
-
   // Attach socket handlers
   playDealServer.onConnected(thisClient);
-
 });
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on(`error`, onError);
-server.on(`listening`, onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (Number.isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server `error` event.
- */
-
-function onError(error) {
-  if (error.syscall !== `listen`) {
-    throw error;
-  }
-
-  var bind = typeof port === `string` ? `Pipe ` + port : `Port ` + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case `EACCES`:
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case `EADDRINUSE`:
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server `listening` event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === `string` ? `pipe ${addr}` : `port ${addr.port}`;
-  debug(`Listening on ${bind}`);
-}
